0c606a480c7a389bd71511c9656e3f80d629a98f
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-06-21 15:26:23 +0200

    feat: initialize mod

---
+++ src/main/java/com/github/sqyyy/fabriccomputers/FabricComputersMod.java
@@ -0,0 +1,10 @@
+package com.github.sqyyy.fabriccomputers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricComputersMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
